053fa7f79355a04c1bd6e0c0ca65125f1c322e1c
phx-hp1000 <306761352@qq.com>
2013-10-02 21:51:47 +0800

    a

--- public/index.php
+++ public/index.php
@@ -1,22 +1,4 @@
 <?php
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylorotwell@gmail.com>
- */
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader
-| for our application. We just need to utilize it! We'll require it
-| into the script here so that we do not have to worry about the
-| loading of any our classes "manually". Feels great to relax.
-|
-*/
 
 require __DIR__.'/../bootstrap/autoload.php';
 
